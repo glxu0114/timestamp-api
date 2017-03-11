76b5b38d723fdc784f9a30130f3987226ee2b8d6
guang xu <g.l-x@hotmail.com>
2017-03-11 05:26:10 +0000

    First Commit.

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var path = require('path');
+var http = require( 'http' );
+var moment = require('moment');
+var app = express();
+var re = new RegExp("^[0-9]*$");
+
+app.set( 'port', process.env.PORT || 3001 );
+
+app.get('/', function (req, res) {
+  
+  res.sendFile(path.join(__dirname +'/index.html'));
+  
+})
+
+app.get('/:data', function(req, res){
+  
+    var date = req.params.data;
+    var stamp = {"unix": null, "natrual": null};
+    
+    if(re.test(date))
+    {
+       //console.log("unix");
+      if(moment(date, "X").isValid())
+      {
+          stamp = {"unix": moment(date, "X").unix(),
+                   "natrual": moment(date, "X").format('MMMM DD, YYYY')
+                  }
+      }
+    }
+    
+    else
+    {
+      if(moment(date, "MMM DD, YYYY").isValid())
+      {
+          stamp = {"unix": moment(date, "MMM DD, YYYY").unix(),
+                   "natrual": moment(date, "MMM DD, YYYY").format('MMMM DD, YYYY')
+                  }
+      }
+    }
+    
+    res.send(JSON.stringify(stamp));
+    
+})
+
+http.createServer( app ).listen( app.get( 'port' ), function (){
+  console.log( 'Express server listening on port ' + app.get( 'port' ));
+});
